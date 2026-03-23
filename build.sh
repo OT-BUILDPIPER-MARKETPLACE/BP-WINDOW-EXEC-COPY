bef697b71b2a8a4a4f65b9d0c66ba6f6ca0445ce
Aayush Verma <aayush.verma@opstree.com>
2026-03-23 17:06:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -99,11 +99,25 @@ ESCAPED_DEST=$(printf '%s' "$DEST_PATH" | sed 's#\\#\\\\#g')
 ESCAPED_BACKUP=$(printf '%s' "$BACKUP_DIR" | sed 's#\\#\\\\#g')
 
 logInfoMessage "Source: $ESCAPED_SOURCE"
-logInfoMessage "Destination: $ESCAPED_DEST"
+
+if [ -n "$ESCAPED_SOURCE" ] && [ -n "$ESCAPED_DEST" ] && [ -n "$ESCAPED_BACKUP" ]; then
+  logInfoMessage "Source: $ESCAPED_SOURCE"
+  logInfoMessage "Destination: $ESCAPED_DEST"
+  add_event "FETCHING SOURCE PATH" "Successful" "Source path retrieved successfully" "Source: $ESCAPED_SOURCE"
+  add_event "FETCHING DESTINATION PATH" "Successful" "Destination path retrieved successfully" "Destination: $ESCAPED_DEST"
+  add_event "FETCHING BACKUP PATH" "Successful" "Backup path retrieved successfully" "Backup: $ESCAPED_BACKUP"
+else
+  logErrorMessage "Source or Destination path is empty"
+   add_event "FETCHING SOURCE PATH" "Failed" "Source or Destination path is missing" "Source: $ESCAPED_SOURCE, Destination: $ESCAPED_DEST, Backup: $ESCAPED_BACKUP"
+  exit 1
+fi
+
+
 
 SOURCE_PATH_ESCAPED=$(printf '%s' "$SOURCE_PATH" | sed 's#\\#\\\\#g')
 DEST_PATH_ESCAPED=$(printf '%s' "$DEST_PATH" | sed 's#\\#\\\\#g')
 BACKUP_DIR_ESCAPED=$(printf '%s' "$BACKUP_DIR" | sed 's#\\#\\\\#g')
+
 
 sleep "${SLEEP_DURATION}"
 
@@ -112,16 +126,20 @@ if [ "$BACKUP_EXISTING" = "true" ]; then
 #   sshpass -p "$TARGET_PASSWORD" ssh -o StrictHostKeyChecking=no "$TARGET_USERNAME@$TARGET_IP" \
 #   "powershell -Command \"\$dt=Get-Date -Format 'yyyyMMdd_HHmmss'; New-Item -ItemType Directory -Path '$BACKUP_DIR_ESCAPED' -Force | Out-Null; \$tarFile='$BACKUP_DIR_ESCAPED\\\\$FOLDER_NAME'+'_'+\$dt+'.tar'; Write-Host 'Creating TAR:' \$tarFile; tar -cvf \$tarFile -C '$SOURCE_PATH_ESCAPED' '$FOLDER_NAME'; Write-Host '===== DONE ====='\""
 
+add_event "RETENTION COUNT" "Successful" "Retention count for backups retrieved successfully" "Retention Count: $RETENTION_COUNT"
+
 sshpass -p "$TARGET_PASSWORD" ssh -o StrictHostKeyChecking=no "$TARGET_USERNAME@$TARGET_IP" \
 "powershell -NoProfile -ExecutionPolicy Bypass -Command \"\$dt=Get-Date -Format 'yyyyMMdd_HHmmss'; New-Item -ItemType Directory -Path '$BACKUP_DIR_ESCAPED' -Force | Out-Null; \$tarFile='$BACKUP_DIR_ESCAPED\\\\$FOLDER_NAME'+'_'+\$dt+'.tar'; Write-Host ('Creating TAR: ' + \$tarFile); tar -cvf \$tarFile -C '$SOURCE_PATH_ESCAPED' '$FOLDER_NAME'; Write-Host ('Applying retention (keep latest $RETENTION_COUNT files)...'); \$files=Get-ChildItem -Path '$BACKUP_DIR_ESCAPED' -Filter '*.tar' | Sort-Object LastWriteTime -Descending; Write-Host ('Total files: ' + \$files.Count); if (\$files.Count -gt $RETENTION_COUNT) { \$files | Select-Object -Skip $RETENTION_COUNT | ForEach-Object { Write-Host ('Deleting: ' + \$_.FullName); Remove-Item -LiteralPath \$_.FullName -Force } } else { Write-Host 'No old backups to delete' }; Write-Host '===== DONE ====='\""
 
   logInfoMessage "Backup completed at $ESCAPED_BACKUP"
+  add_event "ARTIFACT BACKUP PATH" "Successful" "Backup of existing codebase completed successfully" "Backup Location: $ESCAPED_BACKUP"
 else
   logWarningMessage "Skipping backup"
+  add_event "ARTIFACT BACKUP PATH" "Warning" "Backup of existing codebase skipped as BACKUP_EXISTING is not set to true" "Backup Location: $ESCAPED_BACKUP"
 fi
 
 logInfoMessage "Starting robocopy from [$ESCAPED_SOURCE] to [$ESCAPED_DEST]"
-
+add_event "ARTIFACT COPY" "Successful" "Code deployment started using robocopy" "Source: $ESCAPED_SOURCE, Destination: $ESCAPED_DEST"
 
 sshpass -p "$TARGET_PASSWORD" ssh -o StrictHostKeyChecking=no "$TARGET_USERNAME@$TARGET_IP" \
 "robocopy \"$SOURCE_PATH_ESCAPED\\$FOLDER_NAME\" \"$DEST_PATH_ESCAPED\" /E /COPYALL /R:1 /W:1"
@@ -130,7 +148,11 @@ EXIT_CODE=$?
 
 if [ $EXIT_CODE -le 7 ]; then
     TASK_STATUS=0
+add_event "ARTIFACT DEPLOYMENT" "Successful" "Code deployed successfully to target path using robocopy" "Source: $ESCAPED_SOURCE, Destination: $ESCAPED_DEST"
 else
     TASK_STATUS=1
+    logErrorMessage "Robocopy failed with exit code $EXIT_CODE"
+    add_event "ARTIFACT DEPLOYMENT" "Failed" "Code deployment failed during robocopy execution" "Source: $ESCAPED_SOURCE, Destination: $ESCAPED_DEST"
 fi
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
+
